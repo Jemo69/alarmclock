23f63247e9cbf0310d2d96a133456cae18e3219c
Jemo69 <jemolife69@gmail.com>
2024-12-14 17:30:26 -0500

    documentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # alarmclock
+first you will install pygame and then copy the by use git clone and git the image the below
+[github](jemo.png)
--- simple.py
+++ simple.py
@@ -1,16 +1,15 @@
 import time
-import simpleaudio as sa
+import pygame
 
 def alarm(duration, sound_file):
     time.sleep(duration)
+    
+    pygame.mixer.init()
+    pygame.mixer.music.load(sound_file)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
 
-    # Load the sound file
-    wave_obj = sa.WaveObject.from_wave_file(sound_file)
-
-    # Play the sound
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-alarm_time = 3 # Adjust this to your desired time
-sound_file = "sample.wav"  # Replace with your desired sound file
-
-alarm(alarm_time, sound_file)
+alarm_time = 3
+sound_file = "/home/jemo/projects/alarmclock/sample.wav"
+alarm(alarm_time, sound_file)   
